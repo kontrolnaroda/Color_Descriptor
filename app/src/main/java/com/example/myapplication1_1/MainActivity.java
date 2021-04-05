b8954efe98f9747221dad6c3204de7248eabd3ec
kontrolnaroda <kontrolnaroda@mail.ru>
2021-04-06 00:25:05 +0300

    new

---
+++ app/src/main/java/com/example/myapplication1_1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+//nbvnb
+public class MainActivity extends AppCompatActivity {
+private Spinner spinnerColors;
+private TextView descriptionOfTemp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinnerColors = findViewById(R.id.spinnerColors);
+        descriptionOfTemp = findViewById(R.id.descriptionOfTemp);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description = getDescription(position);
+        descriptionOfTemp.setText(description);
+
+    }
+    private String getDescription(int position){
+        String[] getDescriptionP = getResources().getStringArray(R.array.description_of_temp);
+        return getDescriptionP[position];
+    }
+}
